520a8cb1543d4729227ca61be463a6433b813fe0
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-03-03 14:12:39 +0100

    fix comments

--- test.py
+++ test.py
@@ -1,44 +1,14 @@
 """
--		Operands	Count = 78	System.Collections.Generic.List<string>
-		[0]	"import"	string
-		[1]	"hashlib"	string
-		[2]	"def"	string
-		[3]	"hash_file"	string
-		[4]	"("	string
-		[5]	"filename"	string
-		[6]	")"	string
-		[7]	":"	string
-		[8]	"\"\"\"\"This function returns the SHA-1 hash\n   of the file passed into it\"\"\""	string
-		[9]	"h"	string
-		[10]	"hashlib"	string
-		[11]	"."	string
-		[12]	"sha1"	string
-		[13]	"("	string
-		[14]	")"	string
-		[15]	"open"	string
-		[16]	"("	string
-		[17]	"filename"	string
-		[18]	","	string
-		[19]	"'rb'"	string
-		[20]	")"	string
-		[21]	"as"	string
-		[22]	"file"	string
-		[23]	":"	string
-		[24]	"chunk"	string
-
--		Operators	Count = 11	System.Collections.Generic.List<string>
-		[0]	"="	string
-		[1]	"with"	string
-		[2]	"="	string
-		[3]	"while"	string
-		[4]	"!="	string
-		[5]	"="	string
-		[6]	"="	string
-		[7]	"="	string
-		[8]	"lambda"	string
-		[9]	"+"	string
-		[10]	"+"	string 
+test
+sadfsdf
+asdf
+asd
+fasd
+fasd
+fasdf
+asdf
 """
+
 # Python rogram to find the SHA-1 message digest of a file
 
 # importing the hashlib module
